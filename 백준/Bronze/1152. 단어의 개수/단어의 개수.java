b995f7d5a10006f5d91a8ca6bb65f8e7dbba1ac1
Joowon_Yun <114423035+morningB@users.noreply.github.com>
2023-04-02 16:16:44 +0900

    [Bronze II] Title: 단어의 개수, Time: 476 ms, Memory: 31128 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -2,11 +2,17 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-
         String str=s.nextLine();
-        StringTokenizer st= new StringTokenizer(str," ");int cout=0;
-       
-        System.out.println(st.countTokens());
+        int cout=0;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)==' ')
+                cout++;
+        }
+        if(str.charAt(0)==' ' && str.charAt(str.length()-1)==' ')
+            cout--;
+        if(str.charAt(0)!=' ' && str.charAt(str.length()-1)!=' ')
+            cout++;
+        System.out.println(cout);
     }
 }
 
