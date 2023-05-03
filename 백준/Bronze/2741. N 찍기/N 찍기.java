2ba989bec68d4cb506407a63288eb13a8cfe1aa5
Joowon_Yun <114423035+morningB@users.noreply.github.com>
2023-05-03 18:03:25 +0900

    [Bronze V] Title: N 찍기, Time: 1128 ms, Memory: 32524 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.Stack;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        for(int i=1;i<=n;i++)
+            System.out.println(i);
+    }
+}
