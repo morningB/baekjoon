7293f2f41306974e51c6212d8b90167deb11572a
Joowon_Yun <114423035+morningB@users.noreply.github.com>
2023-05-04 22:49:54 +0900

    [Bronze V] Title: 직사각형, Time: 212 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.Stack;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+   Scanner s=new Scanner(System.in);
+   int a=s.nextInt();
+   int b=s.nextInt();
+        System.out.println(a*b);
+   
+    }
+}
