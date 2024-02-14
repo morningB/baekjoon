1e431a05e53729bc54bf20d3e489e4c630439616
Joowon_Yun <114423035+morningB@users.noreply.github.com>
2024-02-14 17:13:11 +0900

    [Silver II] Title: 예산, Time: 60 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Silver II/2512. 예산/예산.py
@@ -0,0 +1,26 @@
+n = int(input())
+
+array = list(map(int, input().split()))
+m = int(input())
+
+
+start = 0
+end = max(array)
+
+while(start <= end):
+    total = 0
+    mid = (start + end) // 2
+
+    for x in array:
+        if x > mid:
+            total += mid
+        else:
+            total += x
+    if total <= m:
+        start = mid+ 1
+    else:
+         
+        end = mid - 1
+
+
+print(end)
