86c445bfd5c0f5ef482edcf15a8842fcbdcb8339
Joowon_Yun <114423035+morningB@users.noreply.github.com>
2025-03-31 14:54:38 +0900

    [level 0] Title: 0 떼기, Time: 0.02 ms, Memory: 3.71 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string n_str) {
+    string answer = "";
+    answer = to_string(stoi(n_str));
+    return answer;
+}
