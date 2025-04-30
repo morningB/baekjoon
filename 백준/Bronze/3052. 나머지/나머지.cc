4f3e6db6456d5418848db3056e21d40e91f254b7
Joowon_Yun <114423035+morningB@users.noreply.github.com>
2025-04-30 16:36:01 +0900

    [Bronze II] Title: 나머지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.cc
+++ 백준/Bronze/3052. 나머지/나머지.cc
@@ -9,15 +9,41 @@ int main()
 {
     // 서로 다른 나머지의 개수
     // 10개 입력은 고정
-    // set을 사용해보자
-    set<int> s;
+    // 배열을 사용해보자
+    // 배열을 사용했을 때
+    // 1. 일단 넣는다.
+    // 배열 안에 같은 값이 있다면 제거
+    // 같은 요소를 체크하기 위한 check 변수
+    // 배열의 사이즈 출력
+    vector<int> arr;
 
     for(int i=0; i<10; i++)
     {
         int tem;
         cin >> tem;
+        bool check = false;
+        int j = 0;
         int r = tem%42;
-        s.insert(r);
+        arr.push_back(r); 
+
+        for(j=0;j<arr.size()-1;j++)
+        {
+            if(r == arr[j] && !arr.empty())
+            {
+                check = true;
+                break;
+            }
+            else
+            {
+
+            }
+        }
+        if(check)
+        {
+            arr.erase(arr.begin() + j);
+        }
+        
     }
-    cout << s.size();
+    cout << arr.size();
+    
 }
