323790e9277319fa948bf65c717325e60a7599fa
Joowon_Yun <114423035+morningB@users.noreply.github.com>
2025-05-22 09:56:44 +0900

    [Silver III] Title: 프린터 큐, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.cc
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.cc
@@ -7,34 +7,35 @@ using namespace std;
 int main() {
     int a;
     cin >> a;
-
-    for(int i = 0; i<a; i++)
+    for (int i = 0;i < a;i++)
     {
-        deque<pair<int,int>> dq;
-        
-        int n,m;
+        int n, m;
+        int cnt = 1;
+
+        deque<pair<int, int>> dq;
         cin >> n >> m;
-        int rec = 0;
-        for(int j=0;j<n;j++)
+
+        for (int j = 0;j < n;j++)
         {
-            int temp; cin >> temp;
-            dq.push_back({j,temp}); // 인덱스와 해당하는 우선순위
+            int tem;
+            cin >> tem;
+            dq.push_back({ j,tem });
         }
-
-        while(1)
+        while (1)
         {
-            int maxDq = 0; // 우선순위를 저장할 변수
-            for(int i=0;i<dq.size();i++) 
-            {
-                int fir = dq.front().first; // 제일 앞에있는 값의 인덱스
-                int sec = dq.front().second;// 제일 앞에있는 값의 우선순위
+            int maxDq = 0;
 
-                maxDq = max(maxDq,sec); // 우선순위 비교
-                dq.pop_front();         // 제일 높은 수가 올때까지 뒤로 이동
-                dq.push_back({fir,sec}); 
+            for (int j = 0;j < dq.size();j++)
+            {
+                if (maxDq < dq.front().second)
+                {
+                    maxDq = dq.front().second;
+                    
+                }
+                dq.push_back({ dq.front().first, dq.front().second });
+                dq.pop_front();
             }
-            // int fir = dq.front().first; // 제일 우선순위 높은 수의 인덱스
-            // int sec = dq.front().second; // 우선순위
+
             if(dq.front().second != maxDq) 
             {
                 dq.push_back(dq.front());
@@ -46,12 +47,12 @@ int main() {
                 else
                 {
                     dq.pop_front();
-                    rec++;
+                    cnt++;
                 }
             }
+
         }
-        cout << rec+1 << endl;
-               
+
+        cout << cnt << endl;
     }
-    
 }
