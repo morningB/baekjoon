720e9dced6f0417d5826236f7e272a04f31271c6
Joowon_Yun <114423035+morningB@users.noreply.github.com>
2025-05-30 10:09:58 +0900

    [Silver II] Title: 에디터, Time: 128 ms, Memory: 20880 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.cc
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -4,37 +4,57 @@
 using namespace std;
 
 int main() {
-    string input;
-    cin >> input;
 
+    // 커서 : 앞 뒤 중간에 존재
+    //문자 길이 L이라면 L+1까지 갈 수 있음
+    // L: 왼쪽(맨앞이라면 무시) / D 오른쪽 이동 / B 왼쪽 문자 삭제 (커서 위치 그대로) / P $ : "$"라는 문자 왼쪽에 추가 ->
+
+    
+    string n;
+    cin >> n;
+    list<char> arr(n.begin(),n.end());
+    
     int m;
     cin >> m;
 
-    list<char> text(input.begin(), input.end());
-    auto cursor = text.end();  // 커서: 마지막을 가리킴
+    auto cursor = arr.end();
 
-    while (m--) {
-        char command;
-        cin >> command;
-
-        if (command == 'L') {
-            if (cursor != text.begin()) cursor--;
-        }
-        else if (command == 'D') {
-            if (cursor != text.end()) cursor++;
-        }
-        else if (command == 'B') {
-            if (cursor != text.begin()) {
-                cursor = text.erase(--cursor);  // erase는 지운 다음 원소를 반환
-            }
-        }
-        else if (command == 'P') {
+    for (int i = 0;i < m;i++)
+    {
+        char ch;
+        cin >> ch;
+        if (ch == 'P')
+        {
             char ch;
             cin >> ch;
-            text.insert(cursor, ch);
+            arr.insert(cursor, ch);
+            
+        }
+        else if (ch == 'L')
+        {
+            
+            if (cursor != arr.begin())
+                cursor--;
+
+        }
+        else if (ch == 'D')
+        {
+            
+            if (cursor != arr.end())
+                cursor++;
+        }
+        else if (ch == 'B')
+        {
+            if (cursor != arr.begin())
+            {
+                cursor = arr.erase(--cursor);
+            }
+
         }
     }
-
-    for (char c : text) cout << c;
+    for(auto a : arr)
+    {
+       cout << a;
+    }
     return 0;
 }
